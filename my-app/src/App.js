f58e7198760cc2e71309ab32dc611926cbdf4a06
HMalein <97528028+HMalein@users.noreply.github.com>
2022-01-18 22:06:38 +0200

    kodune

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+<button>nupp</button>
+<p>Arnold</p>
+<figure>
+  <img src='https://variety.com/wp-content/uploads/2019/10/terminator-1.jpg?w=681&h=383&crop=1&resize=681%2C383'></img>
+</figure>
     </div>
   );
 }
